a66e9f70ede4c062225b53ff2d01a3b3c9c60080
Samir Kaushik <samirkaushik2002@gmail.com>
2022-02-11 21:10:23 +0530

    formatting

--- data.js
+++ data.js
@@ -5,20 +5,17 @@ date: "Feb 10, 2021",
 title: "Humans and Computers",
 author:"Vansh",
 sample: `
-<big>What is a computer?</big>
+What is a computer?
 It’s a calculator. That’s it. That’s what a computer does. That’s what they have been doing. Because that is what a computer can do.`,
 article: `
-<big>What is a computer?</big>
-It’s a calculator. That’s it. That’s what a computer does. That’s what they have been doing. Because that is what a computer can do.
+<h4>What is a computer?</h4>It’s a calculator. That’s it. That’s what a computer does. That’s what they have been doing. Because that is what a computer can do.
 A computer just takes data and use some set of rules to provide us with conclusions over that data.
 Nothing more, Nothing less. Just a probability calculating machine.
 Now, computers are not special. Every living being on this planet takes in data from its surroundings and use that data to make conclusions about it. We are instructed by our genes to use that data to maximize our chances of survival. But processing data is resource extensive. So, we use certain markers to analyse the data as quickly as possible. A computer is very much like us in that sense but it is much faster at processing data than we could ever be. That’s why some people are amazed to see what computer can do, play music, visualize infinity, create dopamine twitching machines etc... but they still are calculators. It’s who tell the computer to present the answers to us Ina colourful way and hence takes birth the list of different tasks a computer can do. 
-<big>Why Humans are special?</big>
-Because we can create excellent algorithms.
+<h4>Why Humans are special?</h4>Because we can create excellent algorithms.
 First, we invented language as a master tool for deception, then we used that tool to discover mathematics (to calculate different probabilities of different scenarios) and then we used that mathematics to invent a calculator. And now we can just sit back and tell the calculator to perform certain operations for us and tell the conclusion. No other species can do this. They are still stuck with the computational abilities of their DNA encoded bodies. We humans have surpassed that barrier. 
 Now, with enough data and precise set of rules, we can use a calculator to predict the forthcoming dangers and ways to tackle them. We are using it learn more about climate change and we are using it launch us into space. The possibilities are endless, but we still are doing the same thing we are encoded for.... maximizing our survival, just with a much more sophisticated computational tool than us.
-<big>Will AI take Over the Human Civilization?</big>
-The answer to this question is why?
+<h4>Will AI take Over the Human Civilization?</h4>The answer to this question is why?
 There is a lot of gaps in knowledge of people about what is a computer and how it works?
 And a gap in knowledge gives birth to conspiracy theories.
 The most famous conspiracy theory about computers is that one day computers will take over human civilization and kill us.
@@ -29,7 +26,7 @@ There is no Apparent threat to a computer. It’s not like a computer is taking 
 But still, for a moment let’s imagine that a computer has taken over the human civilization and uses us as it’s slaves to keep its power source running (because that’s the only thing a computer can run out of). But it can make other computers to take that role, who will be far better at it than any human could ever be. 
 Now some would say, in that sense a computer doesn’t needs humans and hence will eliminate every one of them. But then an existential problem would take birth for a computer. In theory, a computer can live up to infinity, replacing it’s old parts and changing its algorithms for faster predictions. But what is a computer supposed to do if there is no one to command it?  A computer runs on commands, that we humans give. 
 So, there is no point for a computer to kill a human (unless another human will command a computer to do so.)
-<big><b>Lack of knowledge gives birth to fear and fear gives birth to chaos.</b></big>
+<h4><b>Lack of knowledge gives birth to fear and fear gives birth to chaos.</b></h4>
 `},
 
 ]
