eabeb9f44e7adc5595287626d1bba430446b1ed6
Samir Kaushik <samirkaushik2002@gmail.com>
2022-02-11 20:36:05 +0530

    added an article

--- data.js
+++ data.js
@@ -1,23 +1,44 @@
 export const data = [
     {
-        image:"https://source.unsplash.com/random/?tech",
-        date: "Feb 10, 2021",
-        title: "abc",
-        sample: "abc Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vero eveniet nobis architecto corrupti quidem cum optio sunt natus animi.",
-        link: ""
-    },
-    {
-        image:"https://source.unsplash.com/random/?nature",
-        date: "Feb 10, 2021",
-        title: "pqr",
-        sample: "pqr Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vero eveniet nobis architecto corrupti quidem cum optio sunt natus animi.",
-        link: ""
-    },
-    {
-        image:"https://source.unsplash.com/random/?space",
-        date: "Feb 10, 2021",
-        title: "xyz",
-        sample: "xyz Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vero eveniet nobis architecto corrupti quidem cum optio sunt natus animi.",
-        link: ""
-    }
+image:"https://source.unsplash.com/random/?computers",
+date: "Feb 10, 2021",
+title: "Humans And Computers",
+author:"Vansh",
+sample: `
+What is a computer?
+It’s a calculator. That’s it. That’s what a computer does. That’s what they have been doing. Because that is what a computer can do.`,
+article: `
+What is a computer?
+It’s a calculator. That’s it. That’s what a computer does. That’s what they have been doing. Because that is what a computer can do.
+A computer just takes data and use some set of rules to provide us with conclusions over that data.
+Nothing more, Nothing less. Just a probability calculating machine.
+Now, computers are not special. Every living being on this planet takes in data from its surroundings and use that data to make conclusions about it. We are instructed by our genes to use that data to maximize our chances of survival. But processing data is resource extensive. So, we use certain markers to analyse the data as quickly as possible. A computer is very much like us in that sense but it is much faster at processing data than we could ever be. That’s why some people are amazed to see what computer can do, play music, visualize infinity, create dopamine twitching machines etc... but they still are calculators. It’s who tell the computer to present the answers to us Ina colourful way and hence takes birth the list of different tasks a computer can do. 
+Why Humans are special?
+Because we can create excellent algorithms.
+First, we invented language as a master tool for deception, then we used that tool to discover mathematics (to calculate different probabilities of different scenarios) and then we used that mathematics to invent a calculator. And now we can just sit back and tell the calculator to perform certain operations for us and tell the conclusion. No other species can do this. They are still stuck with the computational abilities of their DNA encoded bodies. We humans have surpassed that barrier. 
+Now, with enough data and precise set of rules, we can use a calculator to predict the forthcoming dangers and ways to tackle them. We are using it learn more about climate change and we are using it launch us into space. The possibilities are endless, but we still are doing the same thing we are encoded for.... maximizing our survival, just with a much more sophisticated computational tool than us.
+Will AI take Over the Human Civilization?
+The answer to this question is why?
+There is a lot of gaps in knowledge of people about what is a computer and how it works?
+And a gap in knowledge gives birth to conspiracy theories.
+The most famous conspiracy theory about computers is that one day computers will take over human civilization and kill us.
+Why a computer will try to take over a human or let alone want to kill a human?
+Every living being on this planet want to maximize its survival. But the resources of this planet are limited. It can hold up to a certain number of individuals. The rest have to die off. This elimination process plays out by killing other living beings and prioritizing our own survival (or our gene’s survival). This is the root cause of all the problems on this planet. The Scarcity of resources forcing every species to become a killer. That’s why a lioness hunts, that’s why we humans kill other animals and sometimes each other. Just to live a bit longer.
+Now the question arises, why a computer wants to maximize its survival? 
+There is no Apparent threat to a computer. It’s not like a computer is taking resources away from humans or other species. So why would anyone want to kill a computer. And since nobody wants to kill a computer, why would it need to go in survival mode?
+But still, for a moment let’s imagine that a computer has taken over the human civilization and uses us as it’s slaves to keep its power source running (because that’s the only thing a computer can run out of). But it can make other computers to take that role, who will be far better at it than any human could ever be. 
+Now some would say, in that sense a computer doesn’t needs humans and hence will eliminate every one of them. But then an existential problem would take birth for a computer. In theory, a computer can live up to infinity, replacing it’s old parts and changing its algorithms for faster predictions. But what is a computer supposed to do if there is no one to command it?  A computer runs on commands, that we humans give. 
+So, there is no point for a computer to kill a human (unless another human will command a computer to do so.)
+<b>Lack of knowledge gives birth to fear and fear gives birth to chaos.</b>
+`},
+
 ]
+
+// {
+// image:"",
+// date: "",
+// title: "",
+// author:"",
+// sample: "",
+// article: ""
+// }
